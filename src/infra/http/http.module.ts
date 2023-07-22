cd76aa9b3752f409a64ab32ca2ebf0b7f6577a20
Carlos Molmelstet <molmelstet2016@gmail.com>
2023-07-22 18:55:48 -0300

    feat: Register revenue httpModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,29 +1,31 @@
+import { DeleteExpenseCategoryUseCase } from "@application/expense_category/delete-expense_category.use-case";
+import { FindAllExpenseCategoryUseCase } from "@application/expense_category/find-all-expense_category.use-case";
+import { FindByIdExpenseCategoryUseCase } from "@application/expense_category/find-by-id-expense_category.use-case";
+import { InsertExpenseCategoryUseCase } from "@application/expense_category/insert-expense_category.use-case";
+import { UpdateExpenseCategoryUseCase } from "@application/expense_category/update-expense_category.use-case";
 import { CreateUserUseCase } from "@application/user/create-user.use-case";
+import { FindUserByIdUseCase } from "@application/user/find-user-by-id";
 import { ListAllUsersUseCase } from "@application/user/list-all-Users.use-case";
 import { SignInUseCase } from "@application/user/sign-in.use-case";
 import { SignUpUseCase } from "@application/user/sign-up.use-case";
+import { ExpenseCategoryRepositoryInterface } from "@domain/expense_category/expense_category.repository";
 import { AuthRepositoryInterface } from "@domain/user/auth.repository";
 import { UserRepositoryInterface } from "@domain/user/user.repository";
 import { PrismaService } from "@infra/db/prisma/prisma.service";
+import { PrismaExpenseCategoriesRepository } from "@infra/db/prisma/repositories/prisma-expense_categories-repository";
 import { PrismaUsersRepository } from "@infra/db/prisma/repositories/prisma-users-repository";
 import { Module } from "@nestjs/common";
 import { APP_FILTER, APP_GUARD } from "@nestjs/core";
 import { AuthController } from "./controllers/auth/auth.controller";
-import { UsersController } from "./controllers/users/users.controller";
-import { RolesGuard } from "./guards/roles.guard";
-import { AuthService } from "./services/auth.service";
-import { PrismaExpenseCategoriesRepository } from "@infra/db/prisma/repositories/prisma-expense_categories-repository";
-import { InsertExpenseCategoryUseCase } from "@application/expense_category/insert-expense_category.use-case";
-import { ExpenseCategoryRepositoryInterface } from "@domain/expense_category/expense_category.repository";
-import { ExpenseCategoriesController } from "./controllers/expense_categories/expense_categories.controller";
-import { UpdateExpenseCategoryUseCase } from "@application/expense_category/update-expense_category.use-case";
-import { DeleteExpenseCategoryUseCase } from "@application/expense_category/delete-expense_category.use-case";
-import { FindByIdExpenseCategoryUseCase } from "@application/expense_category/find-by-id-expense_category.use-case";
-import { FindAllExpenseCategoryUseCase } from "@application/expense_category/find-all-expense_category.use-case";
-import { ExpensesProvider } from "./providers/expenses.provider";
 import { ExpenseController } from "./controllers/expense/expense.controller";
+import { ExpenseCategoriesController } from "./controllers/expense_categories/expense_categories.controller";
+import { RevenueController } from "./controllers/revenue/revenue.controller";
+import { UsersController } from "./controllers/users/users.controller";
 import { CustomErrorFilter } from "./filters/error.filter";
-import { FindUserByIdUseCase } from "@application/user/find-user-by-id";
+import { RolesGuard } from "./guards/roles.guard";
+import { ExpensesProvider } from "./providers/expenses.provider";
+import { RevenuesProvider } from "./providers/revenues.provider";
+import { AuthService } from "./services/auth.service";
 
 @Module({
   controllers: [
@@ -31,6 +33,7 @@ import { FindUserByIdUseCase } from "@application/user/find-user-by-id";
     UsersController,
     ExpenseCategoriesController,
     ExpenseController,
+    RevenueController,
   ],
   providers: [
     PrismaService,
@@ -134,6 +137,7 @@ import { FindUserByIdUseCase } from "@application/user/find-user-by-id";
       inject: [PrismaExpenseCategoriesRepository],
     },
     ...ExpensesProvider(),
+    ...RevenuesProvider(),
   ],
 })
 export class HttpModule {}
