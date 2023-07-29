91601d958dc8440d7d83a1ca6fae5876dcbdf50a
Carlos Molmelstet <molmelstet2016@gmail.com>
2023-07-29 19:29:08 -0300

    fix: Ajustando nome da interface InsertRevenueDTO

--- src/application/revenue/insert-revenue.use-case.ts
+++ src/application/revenue/insert-revenue.use-case.ts
@@ -4,14 +4,14 @@ import { RevenueRepositoryInterface } from "@domain/revenue/revenue.repository";
 export class InsertRevenueUseCase {
   constructor(private repository: RevenueRepositoryInterface) {}
 
-  async execute(data: InsertrevenueDTO): Promise<Revenue> {
+  async execute(data: InsertRevenueDTO): Promise<Revenue> {
     const revenueCategory = Revenue.create(data);
     const response = await this.repository.insert(revenueCategory);
     return response.toJSON();
   }
 }
 
-type InsertrevenueDTO = {
+type InsertRevenueDTO = {
   amount: number;
   date: Date;
   userId: string;
