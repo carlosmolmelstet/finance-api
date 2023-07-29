ee92ef5a6a39ba9408cfd6632762bf531d7bddd9
Carlos Molmelstet <molmelstet2016@gmail.com>
2023-07-29 19:29:31 -0300

    feat: Adicionando novas rotas no controller de revenues

--- src/infra/http/controllers/revenue/revenue.controller.ts
+++ src/infra/http/controllers/revenue/revenue.controller.ts
@@ -1,7 +1,17 @@
 import { InsertRevenueUseCase } from "@application/revenue/insert-revenue.use-case";
 import { RequestPayload } from "@helpers/jwt.helper";
-import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthGuard } from "../../guards/auth.guard";
+import { FindAllRevenueUseCase } from "@application/revenue/find-all-revenue.use-case";
+import { FindRevenueByPeriodUseCase } from "@application/revenue/find-revenue-by-period.use-case";
 
 type InsertRevenueDto = {
   amount: number;
@@ -12,7 +22,11 @@ type InsertRevenueDto = {
 @UseGuards(AuthGuard)
 @Controller("revenues")
 export class RevenueController {
-  constructor(private insertRevenueUseCase: InsertRevenueUseCase) {}
+  constructor(
+    private insertRevenueUseCase: InsertRevenueUseCase,
+    private findAllRevenueUseCase: FindAllRevenueUseCase,
+    private findRevenueByPeriodUseCase: FindRevenueByPeriodUseCase
+  ) {}
 
   @Post()
   insert(
@@ -23,5 +37,23 @@ export class RevenueController {
       ...insertRevenueDto,
       userId: request.user.sub,
     });
+  }
+
+  @Get("")
+  findAll(@Req() request: RequestPayload) {
+    return this.findAllRevenueUseCase.execute(request.user.sub);
+  }
+
+  @Get("period")
+  getData(
+    @Req() request: RequestPayload,
+    @Query("startDate") startDate: string,
+    @Query("endDate") endDate: string
+  ) {
+    return this.findRevenueByPeriodUseCase.execute(
+      request.user.sub,
+      new Date(startDate),
+      new Date(endDate)
+    );
   }
 }
