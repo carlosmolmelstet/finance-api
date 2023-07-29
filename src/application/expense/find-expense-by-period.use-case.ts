0a39f0295df15028004dc7de24cee82180b9ceae
Carlos Molmelstet <molmelstet2016@gmail.com>
2023-07-29 19:27:43 -0300

    fix: Ajustando nome da prop

--- src/application/expense/find-expense-by-period.use-case.ts
+++ src/application/expense/find-expense-by-period.use-case.ts
@@ -4,8 +4,16 @@ import { ExpenseRepositoryInterface } from "@domain/expense/expense.repository";
 export class FindExpenseByPeriodUseCase {
   constructor(private repository: ExpenseRepositoryInterface) {}
 
-  async execute(userId: string,startDate: Date, endDate: Date): Promise<Expense[]> {
-    const response = await this.repository.findByPeriod(userId, startDate, endDate);
-    return response.map((expenseCategory) => expenseCategory.toJSON());
+  async execute(
+    userId: string,
+    startDate: Date,
+    endDate: Date
+  ): Promise<Expense[]> {
+    const response = await this.repository.findByPeriod(
+      userId,
+      startDate,
+      endDate
+    );
+    return response.map((expense) => expense.toJSON());
   }
 }
